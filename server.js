17ac53277b018c7a386f0a9fca0e529e620a2dde
Sujal Chaudhary <chsujalm@gmail.com>
2024-03-23 14:49:22 +0530

    minor changes

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-require('dotenv').config({path : "./utils/.env"});
+require('dotenv').config();
 const express = require('express');
 const session = require('express-session');
 const MongoStore = require('connect-mongo');
